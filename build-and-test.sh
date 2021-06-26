cb3e3e6395373aad156396ecce872772f78a45d3
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2021-06-26 01:58:41 -0300

    Update para SQL Server 2019

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,17 +1,8 @@
 clear 
 
-docker build . --no-cache -t registry.oragon.io/library/mssql-server-linux:2019-latest
+docker-compose up --build
 
-docker tag registry.oragon.io/library/mssql-server-linux:2019-latest luizcarlosfaria/mssql-server-linux:2019-latest
+#docker build . --no-cache -t registry.oragon.io/library/mssql-server-linux:2019-latest
 
-docker run \
---rm \
--it \
--e 'ACCEPT_EULA=Y' \
--e 'SA_PASSWORD=8k3W2C2BVzAW9p' \
--e 'MSSQL_DATABASE=db01' \
--e 'MSSQL_DATABASE_COLLATE=SQL_Latin1_General_CP1_CI_AI' \
--e 'MSSQL_USER=bbb' \
--e 'MSSQL_PASSWORD=ccc' \
--p 1433:1433 \
-registry.oragon.io/library/mssql-server-linux:2019-latest
+#docker tag registry.oragon.io/library/mssql-server-linux:2019-latest luizcarlosfaria/mssql-server-linux:2019-latest
+
---
+++ tests/gago.sql
@@ -0,0 +1,24 @@
+USE [db01]
+GO
+
+/****** Object:  Table [dbo].[Teste]    Script Date: 26/06/2021 01:44:17 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Teste](
+	[TesteId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nchar](20) NOT NULL,
+	[Date] [datetime2](7) NULL,
+ CONSTRAINT [PK_Teste] PRIMARY KEY CLUSTERED 
+(
+	[TesteId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+  insert into [db01].[dbo].[Teste] ([Name]) values ('A1');
+  insert into [db01].[dbo].[Teste] ([Name]) values ('A2');
